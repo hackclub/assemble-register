2113b31a37c01e21e7be872b32453f67cab1e5a9
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-06-25 11:19:47 -0700

    fix validation

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -2,19 +2,23 @@ import { registrationsAirtable } from '../../lib/airtable'
 import { requiredList } from '../../lib/manifest'
 
 export default async function Submit (req, res) {
+    const missing = [];
     try {
         for (const item in requiredList) {
             try {
                 if (!requiredList[item](req.body)) {
-                    return res.json({ success: false, error: `You're missing some fields, including ${item}. Please fill in all missing questions.` });
+                    missing.push(item);
                 }
             } catch (err) {
-                
+
             }
         }
     } catch (err) {
         return res.json({ success: false, error: `You're missing some fields. Please fill in all missing questions.` });
     }
+
+    if (missing.length > 5) return res.json({ success: false, error: `You're missing some fields. Please fill in all missing questions.` });
+    if (missing.length) return res.json({ success: false, error: `You are missing some fields: ${missing.map(item => `"${item}"`).join(", ")}` });
 
     try {
         await registrationsAirtable.create(req.body);
